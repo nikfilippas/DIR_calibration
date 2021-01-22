e1f1b65238369a50a07f60e7fc44ffebbd2d5297
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-01-22 05:36:45 +0200

    more tests

--- tests.py
+++ tests.py
@@ -41,3 +41,26 @@ ax.set_xlim(0.0, 0.3)
 ax.set_xlabel("z", fontsize=16)
 ax.set_ylabel("N(z)", fontsize=16)
 fig.savefig("2mpz_comparison.pdf", bbox_inches="tight")
+
+
+# galaxy positions
+import healpy as hp
+from matplotlib.cm import gray, bwr_r
+z_spec, z_phot = xcat["ZSPEC"], xcat["ZPHOTO"]
+ra, dec = xcat["SUPRA"], xcat["SUPDEC"]
+dz = z_spec - z_phot
+eq2gal = (+96.3, -60.1, 0)  # galactic to equatorial rotation
+thin = 50
+data = hp.read_map("/home/nick/Desktop/yxgxk/data/maps/HFI_SkyMap_545_2048_R2.02_full.fits", dtype=float)
+plt.figure(num="mollview", figsize=(12,7))
+hp.mollview(data, fig="mollview", title="", notext=True, xsize=2000,
+            norm="log", cmap=gray, cbar=False,
+            rot=eq2gal, coord="G")
+alphas = z_spec[::thin] / z_spec.mean()
+transp = dz[::thin]/(2*dz.std()) + 2*dz.std()
+colors = bwr_r(transp)
+# colors[:,-1] = alphas
+hp.projscatter(ra[::thin], dec[::thin], coord="C",
+               color=colors, s=2)
+hp.graticule(coord="C", color="y", ls="")
+plt.savefig("2MPZ_zspec_positions.pdf", bbox_inches="tight")
