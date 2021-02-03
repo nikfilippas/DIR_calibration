67cd393dfaaa1e830d67e06ac4208280bd333868
koukoufilippasn <nick.koukoufilippas@physics.ox.ac.uk>
2021-02-03 15:49:15 +0000

    map of full photo-z cat

--- mk_maps.py
+++ mk_maps.py
@@ -8,7 +8,7 @@ from DIR import xref
 # global
 nside = 512
 fname_mask = "data/maps/mask_v3.fits"
-hp.disable_warnings()
+#hp.disable_warnings()
 mask = hp.ud_grade(hp.read_map(fname_mask), nside)
 
 ## 2MPZ ##
@@ -17,10 +17,9 @@ fname_data = "data/cats/2MPZ_FULL_wspec_coma_complete.fits"
 q = xref(fname_data)
 q.remove_galplane(fname_mask, "L", "B")
 q.cutoff("ZPHOTO", [-1.00, 0.10])
-q.cutoff("ZSPEC", -999)
-xcat = q.cat_fid
+cat = q.cat_fid
 
-ipix = hp.ang2pix(nside, xcat["L"], xcat["B"], lonlat=True)
+ipix = hp.ang2pix(nside, cat["L"], cat["B"], lonlat=True)
 npix = hp.nside2npix(nside)
 ngal = np.bincount(ipix, minlength=npix)
 n_mean = np.sum(ngal*mask) / np.sum(mask)
@@ -32,15 +31,13 @@ hp.write_map("data/maps/map_2mpz.fits", delta, overwrite=True)
 zbins = ["wisc%d" % b for b in range(1, 6)]
 fname_data = "data/cats/wiseScosPhotoz160708.csv"
 for i, zbin in enumerate(zbins):
-    print("WIxSC bin %d" % i)
-    fname = fname_data.split(".")[0] + "_bin%d.csv" % i
+    print("WIxSC bin %d" % (i+1))
+    fname = fname_data.split(".")[0] + "_bin%d.csv" % (i+1)
     q = xref(fname)
     q.remove_galplane(fname_mask, "l", "b")
     cat = q.cat_fid
-    q.cutoff("Zspec", -999)
-    xcat = q.cat_fid
-
-    ipix = hp.ang2pix(nside, xcat["l"], xcat["b"], lonlat=True)
+  
+    ipix = hp.ang2pix(nside, cat["l"], cat["b"], lonlat=True)
     npix = hp.nside2npix(nside)
     ngal = np.bincount(ipix, minlength=npix)
     n_mean = np.sum(ngal*mask) / np.sum(mask)
