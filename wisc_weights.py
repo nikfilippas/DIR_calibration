42f22f0b6847d23f16bc0145ed6e215f74fee46f
koukoufilippasn <nick.koukoufilippas@physics.ox.ac.uk>
2021-02-03 11:38:19 +0000

    complete for wisc

--- wisc_weights.py
+++ wisc_weights.py
@@ -7,6 +7,7 @@ fname_mask = "data/mask_v3.fits"
 colors = ["W1c", "W2c", "Bcc", "Rcc"]
 
 for i in range(1, 6):
+    print("WIxSC bin %d" % i)
     # sample selection
     fname = fname_data.split(".")[0] + "_bin%d.csv" % i
     q = DIR_cross_match(fname)
@@ -14,7 +15,8 @@ for i in range(1, 6):
     cat = q.cat_fid
     q.cutoff("Zspec", -999)
     xcat = q.cat_fid
-
     # spectroscopic sample weights
-    weights, _ = DIR_weights(xcat, cat, colors, save="out/weights_wisc%d" % i)
+    print("Finding weights...")
+    weights = DIR_weights(xcat, cat, colors, verbose=True,
+                          save="out/weights_wisc%d" % i)
 
