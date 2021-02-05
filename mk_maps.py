c8cedfc397e1f46c54a43997c8a08866fcf59ed6
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-02-05 23:16:07 +0200

    make g maps (Delta g or not)

--- mk_maps.py
+++ mk_maps.py
@@ -10,22 +10,7 @@ nside = 512
 fname_mask = "data/maps/mask_v3.fits"
 #hp.disable_warnings()
 mask = hp.ud_grade(hp.read_map(fname_mask), nside)
-
-## 2MPZ ##
-print("2MPZ")
-fname_data = "data/cats/2MPZ_FULL_wspec_coma_complete.fits"
-q = xref(fname_data)
-q.remove_galplane(fname_mask, "L", "B")
-q.cutoff("ZPHOTO", [-1.00, 0.10])
-cat = q.cat_fid
-
-ipix = hp.ang2pix(nside, cat["L"], cat["B"], lonlat=True)
-npix = hp.nside2npix(nside)
-ngal = np.bincount(ipix, minlength=npix)
-n_mean = np.sum(ngal*mask) / np.sum(mask)
-delta = mask*(ngal/n_mean - 1)
-hp.write_map("data/maps/map_2mpz.fits", delta, overwrite=True)
-
+is_delta_g = False
 
 ## WIxSC ##
 zbins = ["wisc%d" % b for b in range(1, 6)]
@@ -36,10 +21,43 @@ for i, zbin in enumerate(zbins):
     q = xref(fname)
     q.remove_galplane(fname_mask, "l", "b")
     cat = q.cat_fid
-  
+
     ipix = hp.ang2pix(nside, cat["l"], cat["b"], lonlat=True)
     npix = hp.nside2npix(nside)
     ngal = np.bincount(ipix, minlength=npix)
+    if is_delta_g:
+        n_mean = np.sum(ngal*mask) / np.sum(mask)
+        ngal = mask*(ngal/n_mean - 1)
+        zbin = "Delta_g_" + zbin
+    hp.write_map("data/maps/map_%s.fits" % zbin, ngal, overwrite=True)
+
+
+## 2MPZ ##
+zbin = "2mpz"
+print("2MPZ")
+fname_data = "data/cats/2MPZ_FULL_wspec_coma_complete.fits"
+q = xref(fname_data)
+q.remove_galplane(fname_mask, "L", "B")
+
+# fiducial cut z < 0.10
+q.cutoff("ZPHOTO", [-1.00, 0.10])
+cat = q.cat_fid
+ipix = hp.ang2pix(nside, cat["L"], cat["B"], lonlat=True)
+npix = hp.nside2npix(nside)
+ngal = np.bincount(ipix, minlength=npix)
+if is_delta_g:
+    n_mean = np.sum(ngal*mask) / np.sum(mask)
+    ngal = mask*(ngal/n_mean - 1)
+    zbin = "Delta_g_" + zbin
+hp.write_map("data/maps/map_%s.fits" % zbin, ngal, overwrite=True)
+
+# compare with no z-cut
+cat = q.cat  # original cat without photo-z cuts
+ipix = hp.ang2pix(nside, cat["l"], cat["b"], lonlat=True)
+npix = hp.nside2npix(nside)
+ngal = np.bincount(ipix, minlength=npix)
+if is_delta_g:
     n_mean = np.sum(ngal*mask) / np.sum(mask)
     delta = mask*(ngal/n_mean - 1)
-    hp.write_map("data/maps/map_%s.fits" % zbin, delta, overwrite=True)
+    zbin = "Delta_g_" + zbin
+hp.write_map("data/maps/map_%s_full.fits" % zbin, ngal, overwrite=True)
