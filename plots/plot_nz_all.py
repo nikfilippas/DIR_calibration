e9f9b845b8b57d4a498f41de31c531686bcce86c
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-02-07 02:13:05 +0200

    new N(z) comparison using interpolator

--- plots/plot_nz_all.py
+++ plots/plot_nz_all.py
@@ -14,6 +14,30 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from funcs import Likelihood
 
+# global
+z_arr = np.linspace(0, 1, 1000)
+normed = True  # normed or absolute N(z)'s
+# open every cat and store number of galaxies
+if not normed:
+    print("Finding catalogue sizes...")
+    from DIR import xref
+    fname_mask = "data/maps/mask_v3.fits"
+    sizes = []
+    # 2MPZ
+    fname_data = "data/cats/2MPZ_FULL_wspec_coma_complete.fits"
+    q = xref(fname_data)
+    q.remove_galplane(fname_mask, "L", "B")
+    q.cutoff("ZPHOTO", [-1.00, 0.10])
+    sizes.append(len(q.cat_fid))
+    # WIxSC
+    fname_data = "data/cats/wiseScosPhotoz160708.csv"
+    for i in range(1, 6):
+        fname = fname_data.split(".")[0] + "_bin%d.csv" % i
+        q = xref(fname)
+        q.remove_galplane(fname_mask, "l", "b")
+        sizes.append(len(q.cat_fid))
+
+
 ## N(z)'s in 1909.09102 ##
 zm, Nzm = [], []
 f = np.loadtxt("data/dndz/2MPZ.txt").T
@@ -23,7 +47,7 @@ for b in range(1, 6):
     f = np.loadtxt("data/dndz/WISC_bin%s.txt" % b).T
     zm.append(f[0])
     Nzm.append(f[1])
-
+Nzm = [N*s for N, s in zip(Nzm, sizes)] if not normed else Nzm
 
 ## DIR ##
 # names of z-bins
@@ -45,7 +69,7 @@ fig.tight_layout()
 for col, zz, NN in zip(cols, zm, Nzm):
     ax.plot(zz, NN, c=col, lw=2, ls="--")
 
-for col, zbin in zip(cols, zbins):
+for i, (col, zbin) in enumerate(zip(cols, zbins)):
     print(zbin)
     # load N(z)
     f = np.load("out/DIR_%s.npz" % zbin)
@@ -61,13 +85,15 @@ for col, zbin in zip(cols, zbins):
             Njk += 1
         except FileNotFoundError:
             break
-
-    # run likelihood
+    # error bars
     dNz = np.sqrt(Njk/(Njk-1) * diff_sq)
+    # normalisation
+    Nz = Nz*sizes[i] if not normed else Nz
+    dNz = dNz*sizes[i] if not normed else dNz
+    # run likelihood
     l = Likelihood(z_mid, Nz, dNz)
-    ax.plot(l.z, l.Nz_smooth, c=col, lw=2, label=zbin)
-
+    ax.plot(z_arr, l.Nzi(z_arr), c=col, lw=2, label=zbin)
 
 ax.set_ylim(0,)
-ax.legend(loc="upper right")
+ax.legend(loc="upper right", fontsize=14)
 fig.savefig("img/nz_all.pdf", bbox_inches="tight")
